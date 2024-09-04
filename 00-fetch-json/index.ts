01d1399cdec2fd59bfb0ce1098a829c7ecd22c87
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-04 06:39:24 +0300

    Commit for changes

--- 00-fetch-json/index.ts
+++ 00-fetch-json/index.ts
@@ -1,7 +1,23 @@
 import axios from 'axios';
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todos = response.data;
+  todos.forEach((todo: Todo) => {
+    const id = todo.id;
+    const title = todo.title;
+    const complated = todo.completed;
+    console.log(`
+   The Todo with ID: ${id}
+   Has a title of : ${title}
+   Is it complated? ${complated}
+   `);
+  });
 });
