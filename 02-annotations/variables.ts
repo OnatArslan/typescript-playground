bed7ed35de971d022507f6acf6e9d4f727386b05
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-04 08:01:58 +0300

    Commit for changes

---
+++ 02-annotations/variables.ts
@@ -0,0 +1,10 @@
+let apples: number = 5;
+
+// This will give an error because of type annotations
+// apples = 'hey';
+
+let speed: string = '100';
+
+let hasName: boolean = true;
+
+let nothing: undefined = undefined;
