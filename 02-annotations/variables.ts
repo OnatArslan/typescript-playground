6373ed80b656988a3bab8f8eeb9ddab22d288f3c
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-04 08:09:19 +0300

    Commit for changes

--- 02-annotations/variables.ts
+++ 02-annotations/variables.ts
@@ -1,3 +1,4 @@
+// Primitives -------------------------------
 let apples: number = 5;
 
 // This will give an error because of type annotations
@@ -8,3 +9,15 @@ let speed: string = '100';
 let hasName: boolean = true;
 
 let nothing: undefined = undefined;
+
+// Arrays -------------------------------------
+let colors: string[] = [`red`, `green`, `blue`];
+
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6];
+
+let truths: boolean[] = [true, true, false];
+
+// Classes ----------------------------------
+class Car {}
+
+let car: Car = new Car();
