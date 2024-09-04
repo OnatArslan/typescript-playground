425afdc74a7fcc99cea1078f18bc33e1668188a6
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-04 07:56:09 +0300

    Commit for changes and adding files

---
+++ 01-types/types.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+console.log(today);
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+const red = new Color();
+
+const abbas = 'Abbas' as string;
+
+abbas.padStart(10);
+console.log(abbas.length);
