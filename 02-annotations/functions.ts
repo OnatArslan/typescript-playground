c61cade166a1ab22c86273a5be9a09fe3a39e7cb
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-05 06:22:25 +0300

    Commit for changes

--- 02-annotations/functions.ts
+++ 02-annotations/functions.ts
@@ -1,3 +1,17 @@
+// For arrow function
 const add = (a: number, b: number): number => {
   return a + b;
+};
+// For regular function
+function divide(a: number, b: number): number {
+  return a / b;
+}
+// For ananymous function
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+// never return type is special
+const throwError = (message: string): never => {
+  throw new Error(message);
 };
