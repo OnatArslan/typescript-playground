e73c894662ce4f42d642a4e11ffd509ca1297d19
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-05 06:31:40 +0300

    Commit for changes

--- 02-annotations/functions.ts
+++ 02-annotations/functions.ts
@@ -15,3 +15,38 @@ const multiply = function (a: number, b: number): number {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const forecast = {
+  date: new Date(),
+  weather: `sunny`,
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+// Objects
+const profile = {
+  name: `alex`,
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age }: { age: number } = profile;
+
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
